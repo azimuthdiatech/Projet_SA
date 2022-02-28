e3d79df106e50ab9492ada22514d3edc243dc9f5
azimuthdiatech <diapape446@gmail.com>
2022-02-28 17:03:36 +0000

    first commit

---
+++ public/index.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../public/css/style.css">
+</head>
+<body>
+    <header>
+    <img src="logo.png"  id="logo" alt=""> 
+    <div id="titr"><h3>Le plaisir de jouer</h3></div>
+    </header>
+    <div id="filtre"><img src="background.png" id="bg" alt="">
+        <form action="" method="post">
+            <input type="text" id="login" >
+        </form>
+    </div>
+</body>
+</html>
+<?php
+?>
